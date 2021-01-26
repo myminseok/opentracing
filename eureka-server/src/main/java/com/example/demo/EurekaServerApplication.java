09f3b2492ce6289ec1dfd8c5aa6300c201d8ede1
MinSeok Kim <myminseok@gmail.com>
2021-01-27 01:02:25 +0900

    adding prometheus to eureka

--- eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +16,9 @@ public class EurekaServerApplication {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 }
