5c1af6f91b6389e81f34fe2e0884a4b5c329ded8
MinSeok Kim <myminseok@gmail.com>
2021-01-27 21:48:16 +0900

    reactive retry lb success

--- frontend-lb-webclient/src/main/java/com/example/demo/Application.java
+++ frontend-lb-webclient/src/main/java/com/example/demo/Application.java
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerClientAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.reactive.RetryableLoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,8 @@ public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-//	 @Autowired
-//	 private ReactorLoadBalancerExchangeFilterFunction lbFunction;
+	//@Autowired
+	//private ReactorLoadBalancerExchangeFilterFunction lbFunction; //==> WebClientResponseException$ServiceUnavailable: 503 Service Unavailable from UNKNOWN
 
 	@Bean
 	@LoadBalanced
@@ -37,12 +38,24 @@ public class Application {
 
 	@Autowired
 	private WebClient.Builder webClientBuilder;
-	
 
+
+	/*
+	org.springframework.web.reactive.function.client.WebClientResponseException$ServiceUnavailable: 503 Service Unavailable from UNKNOWN
+	=>
+	If the ReactorLoadBalancerExchangeFilterFunction bean is defined and set it to WebClient.filter()  then, Spring initializes two RoundRobinLoadBalancer object and set  the serviceId  with values from environments with key spring.cloud.loadbalancer.client.name and  one of environments has wrong value(IP) which causes no matching with target instances.
+	in summary, 1) remove ReactorLoadBalancerExchangeFilterFunction bean , 2) set `spring.cloud.loadbalancer.retry.enabled=true.
+	LoadBalancerClientConfiguration
+	LoadBalancerClientFactory
+	RoundRobinLoadBalancer;
+	ReactorLoadBalancerClientAutoConfiguration;
+	RetryableLoadBalancerExchangeFilterFunction
+	 */
 	@RequestMapping( value="/" )
 	public Mono<String> webclient() throws InterruptedException {
+
 		return webClientBuilder.baseUrl("lb://backend")
-				//.filter(lbFunction) //org.springframework.web.reactive.function.client.WebClientResponseException$ServiceUnavailable: 503 Service Unavailable from UNKNOWN
+				//.filter(lbFunction) // WebClientResponseException$ServiceUnavailable: 503 Service Unavailable from UNKNOWN
 				.build()
 				.get()
 				.retrieve().bodyToMono(String.class);
